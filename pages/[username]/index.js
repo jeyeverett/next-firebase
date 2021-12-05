5b95865905d621a053a36177c6e6a5bf9465a2a0
Jeysen Freedman <jeysenfreedman@gmail.com>
2021-12-05 15:30:46 -0400

    add initial routes

---
+++ pages/[username]/index.js
@@ -0,0 +1,7 @@
+export default function UserIndexPage({}) {
+  return (
+    <main>
+      <h1>User Index Page</h1>
+    </main>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link
+        href={{
+          pathname: "/[username]",
+          query: { username: "jeysen" },
+        }}
+      >
+        <a>User Profile</a>
+      </Link>
+    </div>
+  );
 }
