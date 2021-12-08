889f43891f300e97bfd7fc1ddef98ef627a0729a
Jeysen Freedman <jeysenfreedman@gmail.com>
2021-12-08 10:05:00 -0400

    add MetaTags to user posts

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,9 +1,6 @@
-import MetaTags from "../../components/MetaTags";
-
 export default function AdminPostEdit({}) {
   return (
     <main>
-      <MetaTags title="admin page" />
       <h1>Edit Post</h1>
     </main>
   );
